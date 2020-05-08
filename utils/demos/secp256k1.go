82e2e8f781877eec9c368d40a9daccc3f4681273
lhfbc <lhf_bc@163.com>
2020-05-08 17:00:57 +0800

    add demos of secp256k1

---
+++ utils/demos/secp256k1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/triasteam/StreamNet-go/utils/crypto"
+	"github.com/triasteam/StreamNet-go/utils/crypto/secp256k1"
+)
+
+func main() {
+	privKey := secp256k1.GenPrivKey()
+	pubKey := privKey.PubKey()
+
+	msg := crypto.CRandBytes(128)
+	sig, err := privKey.Sign(msg)
+	println("err = ", err)
+	//require.Nil(t, err)
+
+	//assert.True(t, pubKey.VerifyBytes(msg, sig))
+	r := pubKey.VerifyBytes(msg, sig)
+	println(r)
+
+	// Mutate the signature, just one bit.
+	sig[3] ^= byte(0x01)
+	//assert.False(t, pubKey.VerifyBytes(msg, sig))
+	r = pubKey.VerifyBytes(msg, sig)
+	println(r)
+}
