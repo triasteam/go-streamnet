239b4920da04ab5de1b03fdb396508d381549a41
lhfbc <lhf_bc@163.com>
2020-07-01 15:56:13 +0800

    modify the interface

--- store/provider.go
+++ store/provider.go
@@ -1,17 +1,20 @@
 package store
 
-import "github.com/triasteam/go-streamnet/types"
-
 type StorageProvider interface {
-	init()
-	isAvailable() bool
-	shutdown()
-    save(key types.Hash, value []byte) bool
-	delete(key types.Hash, value []byte)
-	update(key types.Hash, value []byte)
-    exists(key types.Hash, value []byte) bool
-	saveBatch() bool
-    deleteBatch()
-	get()
-	mayExist() bool
+	Init(path string) error
+	Save(key []byte, value []byte) error
+	SaveValue(value []byte) ([]byte, error)
+	Get(key []byte) ([]byte, error)
+
+	/*
+		isAvailable() bool
+		shutdown()
+		delete(key types.Hash, value []byte)
+		update(key types.Hash, value []byte)
+		exists(key types.Hash, value []byte) bool
+		saveBatch() bool
+		deleteBatch()
+		mayExist() bool
+	*/
+
 }
