14eb6d91cb2c52fbd299662eeae3ea23932b96e1
lhfbc <lhf_bc@163.com>
2020-08-10 19:46:49 +0800

    implement type 'Set'

--- types/transaction.go
+++ types/transaction.go
@@ -2,6 +2,7 @@ package types
 
 // Transaction is a core struct.
 type Transaction struct {
+<<<<<<< HEAD
 	bytes   []byte
 	address Hash
 
@@ -56,4 +57,23 @@ func (tx *Transaction) GetApprovers() Set {
 	s := NewSet()
 
 	return s
+=======
+	bytes                         []byte
+	address                       Hash
+	trunk                         Hash
+	branch                        Hash
+	obsoleteTag                   Hash
+	value                         int64
+	currentIndex                  int64
+	lastIndex                     int64
+	timestamp                     int64
+	tag                           Hash
+	attachmentTimestamp           int64
+	attachmentTimestampLowerBound int64
+	attachmentTimestampUpperBound int64
+	height                        int64
+	sender                        string
+	weightMagnitude               int64
+	nonce                         []byte
+>>>>>>> 688dc7a... implement type 'Set'
 }
