4d700bd053b4403addaa7f0f76d6d9e84e2eecaf
lhfbc <lhf_bc@163.com>
2020-08-10 20:01:52 +0800

    modify Set to store Hash instead of int.

--- types/set.go
+++ types/set.go
@@ -6,7 +6,6 @@ import (
 
 // Set implement 'set' struct.
 type Set struct {
-<<<<<<< HEAD
 	m map[Hash]bool
 	sync.RWMutex
 }
@@ -15,31 +14,16 @@ type Set struct {
 func NewSet() Set {
 	return Set{
 		m: map[Hash]bool{},
-=======
-	m map[int]bool
-	sync.RWMutex
-}
-
-// New creates a new set.
-func New() *Set {
-	return &Set{
-		m: map[int]bool{},
->>>>>>> 688dc7a... implement type 'Set'
 	}
 }
 
 // Add adds new item to set.
-<<<<<<< HEAD
 func (s Set) Add(item Hash) {
-=======
-func (s *Set) Add(item int) {
->>>>>>> 688dc7a... implement type 'Set'
 	s.Lock()
 	defer s.Unlock()
 	s.m[item] = true
 }
 
-<<<<<<< HEAD
 // AddAll adds all items to set.
 func (s Set) AddAll(items Set) {
 	s.Lock()
@@ -51,21 +35,13 @@ func (s Set) AddAll(items Set) {
 
 // Remove deletes an item from set.
 func (s Set) Remove(item Hash) {
-=======
-// Remove deletes an item from set.
-func (s *Set) Remove(item int) {
->>>>>>> 688dc7a... implement type 'Set'
 	s.Lock()
 	s.Unlock()
 	delete(s.m, item)
 }
 
 // Has checks whether an item is in set.
-<<<<<<< HEAD
 func (s Set) Has(item Hash) bool {
-=======
-func (s *Set) Has(item int) bool {
->>>>>>> 688dc7a... implement type 'Set'
 	s.RLock()
 	defer s.RUnlock()
 	_, ok := s.m[item]
@@ -73,16 +49,11 @@ func (s *Set) Has(item int) bool {
 }
 
 // Len returns the length of set.
-<<<<<<< HEAD
 func (s Set) Len() int {
-=======
-func (s *Set) Len() int {
->>>>>>> 688dc7a... implement type 'Set'
 	return len(s.List())
 }
 
 // Clear removes all items from set.
-<<<<<<< HEAD
 func (s Set) Clear() {
 	s.Lock()
 	defer s.Unlock()
@@ -91,16 +62,6 @@ func (s Set) Clear() {
 
 // IsEmpty checks whether the set is empty.
 func (s Set) IsEmpty() bool {
-=======
-func (s *Set) Clear() {
-	s.Lock()
-	defer s.Unlock()
-	s.m = map[int]bool{}
-}
-
-// IsEmpty checks whether the set is empty.
-func (s *Set) IsEmpty() bool {
->>>>>>> 688dc7a... implement type 'Set'
 	if s.Len() == 0 {
 		return true
 	}
@@ -108,17 +69,10 @@ func (s *Set) IsEmpty() bool {
 }
 
 // List returns all items in the set.
-<<<<<<< HEAD
 func (s Set) List() []Hash {
 	s.RLock()
 	defer s.RUnlock()
 	list := []Hash{}
-=======
-func (s *Set) List() []int {
-	s.RLock()
-	defer s.RUnlock()
-	list := []int{}
->>>>>>> 688dc7a... implement type 'Set'
 	for item := range s.m {
 		list = append(list, item)
 	}
