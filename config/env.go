2ab6824ab771afe1264dd644efa07e5cc5fcb817
niming2100 <niming_2009@163.com>
2020-08-10 16:52:00 +0800

    [fix #25] fix bugs

--- config/env.go
+++ config/env.go
@@ -1,12 +1,24 @@
 package config
 
-import (
+import(
+	"io/ioutil"
 	"gopkg.in/yaml.v2"
 )
 
-var EnvConfig StreamNetconf{}
+var EnvConfig = StreamNetconf{ //default value
+	DBPath: "./db",
+	Port: "14700",
+}
 
 type StreamNetconf struct {
 	DBPath     string  `yaml:"DBPath"`
 	Port       string  `yaml:"Port"`
+}
+
+func init(){
+	var filePath = "config.yml";
+	data, err := ioutil.ReadFile(filePath);
+	if(err == nil) {
+		yaml.Unmarshal(data, &EnvConfig)
+	}
 }
