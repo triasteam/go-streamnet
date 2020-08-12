5aaec1ec6559078e0bfbb78b3b9e1f651aeb3fd9
niming2100 <niming_2009@163.com>
2020-08-12 17:22:10 +0800

    [fix #16] dev

--- config/env.go
+++ config/env.go
@@ -7,12 +7,32 @@ import(
 
 var EnvConfig = StreamNetconf{ //default value
 	DBPath: "./db",
-	Port: "14700",
+	Port: ":14700",
+	Redis: redisConfig,
 }
 
+var redisConfig = RedisConfig{ //default value
+	Url: "localhost:6379",
+	Password: "",
+	DB: 0,
+}
+
+type RedisConfig struct {
+	Url 		string	`yaml:"Url"`
+	Password 	string	`yaml:"Password"`
+	DB 			int		`yaml:"DB"`
+}
+/*
+
+Redis: 
+  Port: 6379
+  Password:
+  DB: 0
+*/
 type StreamNetconf struct {
-	DBPath     string  `yaml:"DBPath"`
-	Port       string  `yaml:"Port"`
+	DBPath		string		`yaml:"DBPath"`
+	Port		string		`yaml:"Port"`
+	Redis		RedisConfig	`yaml:"Redis"`
 }
 
 func init(){
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/triasteam/go-streamnet
 
 go 1.14
 
-require github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c
+	gopkg.in/yaml.v2 v2.3.0
+)
