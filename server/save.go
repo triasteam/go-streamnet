bc3b3ba4d5f0fbfe8c7359089721423a7378c920
lhfbc <lhf_bc@163.com>
2020-08-29 15:27:58 +0800

    modify 'save' flow

--- server/save.go
+++ server/save.go
@@ -1,37 +1,84 @@
 package server
 
 import (
+	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/triasteam/go-streamnet/config"
+
+	"github.com/triasteam/go-streamnet/abci/proto"
 
 	"github.com/triasteam/go-streamnet/types"
+	"google.golang.org/grpc"
 )
 
-func StoreMessage(message *types.StoreData) error {
+func callApp(data string) string {
+	// create connection
+	conn, err := grpc.Dial(address+":"+rpcPort, grpc.WithInsecure())
+	if nil != conn {
+		defer conn.Close()
+	}
+
+	if nil != err {
+		fmt.Printf("Connect to grpc server failed: %s\n", err)
+		return ""
+	}
+
+	client := proto.NewStreamnetServiceClient(conn)
+
+	req := &proto.RequestStoreBlock{
+		BlockInfo: data,
+	}
+
+	result, _ := client.StoreBlock(context.Background(), req)
+	if nil != result {
+		fmt.Printf("%s \n", result.Data)
+	} else {
+		fmt.Println("response is nil")
+	}
+
+	return result.Data
+}
+
+func StoreMessage(message *types.StoreData) ([]byte, error) {
+
 	// Tipselection
 	// Check genesis
-	txToApprove := sn.Tips.GetTransactionsToApprove(15, types.NilHash)
+	//txsToApprove := sn.Tips.GetTransactionsToApprove(15, types.NilHash)
+	txsToApprove := types.List{}
+	txsToApprove.Append(config.GenesisTrunk)
+	txsToApprove.Append(config.GenesisBranch)
 
 	// Transaction
 	tx := types.Transaction{}
-	tx.Init(txToApprove)
+	tx.Init(txsToApprove)
 
-	// Hash
+	// grpc
+	grpcResult := callApp(message.String())
+	h := types.NewHashHex(grpcResult)
+	tx.DataHash = h
+	log.Printf("Grpc result: %s\n", h)
 
 	// POW
+
+	// timestamp
+	tx.Timestamp = time.Now()
 
 	// Save to dag
 
 	// Save to db
-	/*k, err := db.SaveValue([]byte(message.String()))
+	k, err := sn.Store.SaveValue([]byte(tx.String()))
 	if err != nil {
 		log.Printf("Save data to database failed: %v\n", err)
-		fmt.Fprintf(w, `{"code":-1, "hash": }`)
-		return
-	}*/
-	return nil
+	}
+	log.Printf("Store to database successed!\n")
+
+	return k, err
 }
 
 // SaveHandle process the 'save' request.
@@ -48,15 +95,15 @@ func SaveHandle(w http.ResponseWriter, r *http.Request) {
 	log.Printf("POST json: Attester=%s, Attestee=%s\n", params.Attester, params.Attestee)
 
 	// save data to dag & db
-	err = StoreMessage(&params)
+	key, err := StoreMessage(&params)
 	if err != nil {
 		fmt.Printf("Save message error: %v.", err)
 		return
 	}
 
-	/*// hex encode
-	key_hex := make([]byte, hex.EncodedLen(len(k)))
-	hex.Encode(key_hex, k)
+	// hex encode
+	key_hex := make([]byte, hex.EncodedLen(len(key)))
+	hex.Encode(key_hex, key)
 
 	// return
 	store_reply := types.StoreReply{
@@ -64,5 +111,5 @@ func SaveHandle(w http.ResponseWriter, r *http.Request) {
 		Hash: fmt.Sprintf("0x%s", key_hex),
 	}
 	reply, _ := json.Marshal(store_reply)
-	w.Write(reply)*/
+	w.Write(reply)
 }
