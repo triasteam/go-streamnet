22242f001944e79cce8a929bbe0edafa6abba268
lhfbc <lhf_bc@163.com>
2020-08-30 17:42:18 +0800

    save is ok.

---
+++ dag/load_dag.go
@@ -0,0 +1,13 @@
+package dag
+
+// Rebuild dag using database.
+func (dag *Dag) load() {
+}
+
+// todo: restart
+func computeToplogicalOrder() {
+}
+
+// todo: restart
+func (dag *Dag) ComputeScore() {
+}
