f524bc65e809ac261e5e92257f6a077314966be1
lhfbc <lhf_bc@163.com>
2020-10-19 17:05:53 +0800

    config relay port.

--- scripts/relay/relay.go
+++ scripts/relay/relay.go
@@ -8,14 +8,18 @@ import (
 	circuit "github.com/libp2p/go-libp2p-circuit"
 )
 
+const RELAY_PORT string = "14701"
+
 // can build a relay node
 func main() {
 	// fmt.Println("Opt hop will start")
-	h2, err := libp2p.New(context.Background(), libp2p.EnableRelay(circuit.OptHop))
+	h2, err := libp2p.New(context.Background(),
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/"+RELAY_PORT),
+		libp2p.EnableRelay(circuit.OptHop))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v/p2p/%s\n", h2.Addrs()[1], h2.ID().Pretty())
+	fmt.Printf("%+v/p2p/%s\n", h2.Addrs(), h2.ID().Pretty())
 
 	select {}
 }
