1327fe2a9f14a2d0eda275fbaa797c19678b5784
jdyuankai <yuank1987@163.com>
2020-12-15 13:23:09 +0800

    gossipsub use auto relay

---
+++ examples/libp2p/gossipsub-with-relay/flags.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+)
+
+// Config contains ...
+type Config struct {
+	Seed     string
+	Port     string
+	UseRelay bool
+}
+
+// ParseFlags parsing arguments for app
+func ParseFlags() (Config, error) {
+	config := Config{}
+	flag.StringVar(&config.Seed, "seed", "/ip4/127.0.0.1/tcp/45759/ipfs/QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d", "while starting you peer, a seed should be specify")
+	flag.StringVar(&config.Port, "port", "45759", "listening port")
+	flag.BoolVar(&config.UseRelay, "relay", false, "enable relay")
+	flag.Parse()
+
+	return config, nil
+}
--- go.mod
+++ go.mod
@@ -22,7 +22,7 @@ require (
 	github.com/libp2p/go-libp2p v0.4.2
 	github.com/libp2p/go-libp2p-circuit v0.1.4
 	github.com/libp2p/go-libp2p-connmgr v0.2.0 // indirect
-	github.com/libp2p/go-libp2p-core v0.2.5
+	github.com/libp2p/go-libp2p-core v0.7.0
 	github.com/libp2p/go-libp2p-kad-dht v0.3.0
 	github.com/libp2p/go-libp2p-mplex v0.2.1
 	github.com/libp2p/go-libp2p-pnet v0.1.0 // indirect
