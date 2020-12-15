1d707cbcb220de7d1c34ad1c07795b7b10f5c9f6
jdyuankai <yuank1987@163.com>
2020-12-15 16:05:17 +0800

    auto refresh

--- examples/libp2p/gossipsub-with-relay/flags.go
+++ examples/libp2p/gossipsub-with-relay/flags.go
@@ -6,9 +6,10 @@ import (
 
 // Config contains ...
 type Config struct {
-	Seed     string
-	Port     string
-	UseRelay bool
+	Seed       string
+	Port       string
+	UseRelay   bool
+	PublicAddr string
 }
 
 // ParseFlags parsing arguments for app
@@ -17,6 +18,7 @@ func ParseFlags() (Config, error) {
 	flag.StringVar(&config.Seed, "seed", "/ip4/127.0.0.1/tcp/45759/ipfs/QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d", "while starting you peer, a seed should be specify")
 	flag.StringVar(&config.Port, "port", "45759", "listening port")
 	flag.BoolVar(&config.UseRelay, "relay", false, "enable relay")
+	flag.StringVar(&config.PublicAddr, "public", "", "public address, as \"154.8.160.48\"")
 	flag.Parse()
 
 	return config, nil
