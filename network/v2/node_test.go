12e7844e58fff890c1921df0cf2c09fcf4bf1da7
jdyuankai <yuank1987@163.com>
2021-01-21 15:54:00 +0800

    optimize unit test coverage to 83.3% of node.go

--- network/v2/node_test.go
+++ network/v2/node_test.go
@@ -3,119 +3,100 @@ package networkv2
 import (
 	"context"
 	"fmt"
+	"os"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
 	secio "github.com/libp2p/go-libp2p-secio"
+	"github.com/libp2p/go-libp2p/p2p/host/relay"
 	localConfig "github.com/triasteam/go-streamnet/config"
 )
 
-func TestNewNodeWithNoParam(t *testing.T) {
+func TestNewNode(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	cfg, _ := localConfig.ParseFlags()
+	//************ init param  ***********
+	relay.AdvertiseBootDelay = 1 * time.Second
 
-	var receive func(b []byte) error
-	_, err := NewNode(ctx, &cfg, receive)
-	if err != nil {
-		t.Fatal(err)
+	var result []byte
+	OnReceived := func(b []byte) error {
+		result = b
+		return nil
 	}
-}
 
-func TestNewNodeWithSeed(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	err := os.Remove("priv.pem")
+	if err != nil {
+		t.Logf("priv.pem not exists, ignore this.")
+	}
+	//************ init param  ***********
 
+	// 1. Create a seed node, only responsible for bootstrap initing a network
 	security := libp2p.Security(secio.ID, secio.New)
 	seed, err := libp2p.New(ctx, security)
 	seedAddr := fmt.Sprintf("%s/p2p/%s", seed.Addrs()[1].String(), seed.ID().Pretty())
 
-	cfg := &localConfig.Config{}
-	cfg.Seed = seedAddr
-	cfg.Port = "8800"
+	time.Sleep(1 * time.Second)
 
-	var receive func(b []byte) error
-	_, err = NewNode(ctx, cfg, receive)
+	// 2. Create a relay hop
+	hopCfg := &localConfig.Config{}
+	hopCfg.Seed = seedAddr
+	hopCfg.Port = "8800"
+	hopCfg.RelayType = "hop"
+
+	_, err = NewNode(ctx, hopCfg, OnReceived)
 	if err != nil {
 		t.Fatal(err)
 	}
+	os.Remove("priv.pem")
 
-	// seed与new node建立连接，其addr book长度等于2
-	if len(seed.Peerstore().Peers()) != 2 {
-		t.Fatal("new node didn't connect to seed.")
-	}
-}
+	time.Sleep(1 * time.Second)
 
-func TestNewNodeWithAutoRelay(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	// 3. Create a normal node need relay to join the network
+	needRelayConf := &localConfig.Config{}
+	needRelayConf.Seed = seedAddr
+	needRelayConf.Port = "8801"
+	needRelayConf.RelayType = "autorelay"
 
-	seed, err := libp2p.New(ctx)
-	seedAddr := fmt.Sprintf("%s/p2p/%s", seed.Addrs()[0].String(), seed.ID().Pretty())
-
-	cfg, err := localConfig.ParseFlags()
-	cfg.Seed = seedAddr
-
-	// define relay hop
-	cfg.RelayType = "hop"
-	var receive func(b []byte) error
-	_, err = NewNode(ctx, &cfg, receive)
+	needRelayNode, err := NewNode(ctx, needRelayConf, OnReceived)
 	if err != nil {
 		t.Fatal(err)
 	}
+	os.Remove("priv.pem")
 
-	// define relay
-	cfg.RelayType = "autorelay"
-	_, err = NewNode(ctx, &cfg, receive)
+	time.Sleep(1 * time.Second)
+
+	// 4. Create a normal node need relay to join the network
+	anotherNeedRelayConf := &localConfig.Config{}
+	anotherNeedRelayConf.Seed = seedAddr
+	anotherNeedRelayConf.Port = "8802"
+	anotherNeedRelayConf.RelayType = "autorelay"
+
+	_, err = NewNode(ctx, anotherNeedRelayConf, OnReceived)
 	if err != nil {
 		t.Fatal(err)
 	}
+	os.Remove("priv.pem")
 
-}
+	time.Sleep(1 * time.Second)
 
-func TestBroadcast(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	message := []byte("hello")
+	needRelayNode.Broadcast(message)
 
-	security := libp2p.Security(secio.ID, secio.New)
-	seed, err := libp2p.New(ctx, security)
-	seedAddr := fmt.Sprintf("%s/p2p/%s", seed.Addrs()[0].String(), seed.ID().Pretty())
-
-	cfg, err := localConfig.ParseFlags()
-	cfg.Seed = seedAddr
-
-	// var haveReceived bool
-	// define relay hop
-	receive := func(b []byte) error {
-		// haveReceived = true
-		if string(b) != "hello" {
-			t.Fatal("not hello !")
-		}
-		return nil
+	// seed与new node建立连接，其addr book长度等于4
+	if len(seed.Peerstore().Peers()) != 4 {
+		t.Fatalf("expected 4, actural %d", len(seed.Network().Peers()))
 	}
 
-	var fun func([]byte) error
-	_, err = NewNode(ctx, &cfg, fun)
-	if err != nil {
-		t.Fatal(err)
+	time.Sleep(10 * time.Second)
+
+	if reflect.DeepEqual(result, message) {
+		t.Log("successed!")
+	} else {
+		t.Fatalf("expected %s, actural %s", message, result)
 	}
-
-	cfg.Port = "45758"
-	_, err = NewNode(ctx, &cfg, receive)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// time.Sleep(1 * time.Second)
-
-	// sender.Broadcast([]byte("hello"))
-
-	// time.Sleep(1 * time.Second)
-
-	// if haveReceived == false {
-	// 	t.Fatal("haven't received message !")
-	// }
-
-	// time.Sleep(1 * time.Second)
+	ctx.Done()
+	return
 }
